d7ebefd63690cf9d4adc210d8989800d20a0311a
heeboy007 <41155496+heeboy007@users.noreply.github.com>
2024-04-11 17:20:02 +0900

    service: npm run product to stat static server

---
+++ server.cjs
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require('http');
+//const https = require('https');
+const fs = require('fs');
+
+const app = express();
+
+// Serve static files from the 'dist' directory
+app.use(express.static('dist'));
+
+// HTTP server
+const httpServer = http.createServer(app);
+
+// HTTPS server
+// const privateKey = fs.readFileSync('path/to/private.key', 'utf8'); // Replace 'path/to/private.key' with the path to your private key file
+// const certificate = fs.readFileSync('path/to/certificate.crt', 'utf8'); // Replace 'path/to/certificate.crt' with the path to your certificate file
+// const credentials = { key: privateKey, cert: certificate };
+// const httpsServer = https.createServer(credentials, app);
+
+// Start HTTP server
+httpServer.listen(80, () => {
+    console.log('HTTP server running on port 80');
+});
+
+// Start HTTPS server
+// httpsServer.listen(443, () => {
+//     console.log('HTTPS server running on port 443');
+// });
